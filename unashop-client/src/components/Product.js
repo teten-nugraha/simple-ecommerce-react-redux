35ec65421a3b213459a0ceb7e01ed1e9756d926d
Teten Nugraha <teten.nugraha@allianz.co.id>
2021-06-21 09:51:39 +0700

    update product detail page

--- unashop-client/src/components/Product.js
+++ unashop-client/src/components/Product.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import Rating from 'react-rating';
 
-export default function Product({product}) {
+export default function Product({ product }) {
   return (
-      <div className="col-md-3 m-5">
-          <img src={product.image} className='img-fluid' />
-          <h1>{product.name}</h1>
-          <h1>Rating: {product.rating}</h1>
-          <h1>Price : {product.price}</h1>
+    <div className="col-md-3 m-5 card p-2 text-left">
+      <div>
+        <Link to={`product/${product.id}`}>
+        <img src={product.image} className='img-fluid' />
+        <h1>{product.name}</h1>
+        <Rating
+          style={{color:'orange'}}
+          initialRating={product.rating}
+          emptySymbol="fa fa-star-o fa-1x"
+          fullSymbol="fa fa-star fa-1x"
+        />
+        <h1>Price : {product.price}</h1>
+        </Link>
       </div>
+    </div>
   )
 }
--- unashop-client/src/screens/ProductScreen.js
+++ unashop-client/src/screens/ProductScreen.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import products from '../products';
 
-export default function ProductScreen() {
+export default function ProductScreen({ match }) {
+
+  const productId = match.params.id;
+
+  const product = products.find(product => product.id == productId)
+
   return (
     <div>
-        <h1>Product Screen</h1>
+      <div className="row" >
+        <div className="col-md-6">
+          <div className="card p-5 m-2">
+            <h1>{product.name}</h1>
+            <img src={product.image} className="img-fluid m-3 bigimg" />
+            <p>
+              {product.description}
+            </p>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="m-2">
+            <h1>Price : {product.price}</h1>
+            <hr />
+            <h1>Select Quantity</h1>
+            <select>
+              {[...Array(product.countInStock).keys()].map((x, i) => {
+                return <option value={i+1}>{i+1}</option>
+              })}
+            </select>
+            <hr/>
+            <button className='btn btn-dark'>ADD TO CART</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
